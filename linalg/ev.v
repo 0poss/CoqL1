cfdeab0560d7663488868b7878703c5df118ba9a
0poss <brnnrlxndr@gmail.com>
2022-09-09 17:45:33 +0200

    Done ex1. Started ex2.

--- linalg/ev.v
+++ linalg/ev.v
@@ -1,27 +1,92 @@
 Require Import
-  Coq.Classes.Morphisms
   MathClasses.interfaces.abstract_algebra
   MathClasses.interfaces.vectorspace
-  MathClasses.misc.workaround_tactics
-  MathClasses.theory.setoids
   MathClasses.theory.groups.
 
-Lemma f_equiv' `{Equiv A} `{f : A -> A} :
-  f = f -> forall x y, x = y -> f x = f y.
-Proof.
-  intros.
-  f_equiv.
-  assumption.
-Qed.
+Module Exercise1.
+  Lemma f_equiv' `{Equiv A} `{f : A -> A} :
+    f = f -> forall x y, x = y -> f x = f y.
+  Proof.
+    intros.
+    f_equiv.
+    assumption.
+  Qed.
 
-Goal forall `{HVS : VectorSpace K V}, forall u : V, 0 · u = mon_unit.
-Proof.
-  intros.
-  setoid_rewrite <- left_identity.
-  setoid_rewrite <- left_inverse with (x := 1 · u).
-  setoid_rewrite <- associativity.
-  apply f_equiv'.
-  { cbv; intros ?? Hxy; now rewrite Hxy. }
-  setoid_rewrite <- distribute_r.
-  cbv; now group.
-Qed.
+  Lemma one : forall `{HVS : VectorSpace K V}, forall (u : V), 0 · u = mon_unit.
+  Proof.
+    intros.
+    setoid_rewrite <- left_identity.
+    setoid_rewrite <- left_inverse with (x := 1 · u).
+    setoid_rewrite <- associativity.
+    apply f_equiv'.
+    { cbv; intros ?? Hxy; now rewrite Hxy. }
+    setoid_rewrite <- distribute_r.
+    cbv; now group.
+  Qed.
+
+  Lemma two : forall `{HVS : VectorSpace K V}, forall (α : K), α · mon_unit = mon_unit.
+  Proof.
+    intros.
+    rewrite <- right_identity.
+    rewrite <- (right_inverse (α · mon_unit)) at 2 3.
+    rewrite associativity.
+    apply (f_equiv' (f := fun v => v & - (α · mon_unit)));
+      [ cbv; intros ?? Hxy; now rewrite Hxy |].
+    rewrite <- distribute_l.
+    pose scalar_mult_proper.
+    rewrite left_identity.
+    reflexivity.
+  Qed.
+
+  Lemma three : forall `{HVS : VectorSpace K V}, forall (u : V), -1 · u = -u.
+  Proof.
+    intros.
+    setoid_rewrite <- right_identity.
+    setoid_rewrite <- (right_inverse u).
+    setoid_rewrite associativity.
+    apply (f_equiv' (f := fun v => v & - u));
+      [ cbv; intros ?? Hxy; now rewrite Hxy |].
+    rewrite left_inverse.
+    rewrite <- (left_identity u (op := (·))) at 2.
+    rewrite <- distribute_r.
+    pose scalar_mult_proper.
+    rewrite left_inverse.
+    apply one.
+  Qed.
+
+End Exercise1.
+
+Module Exercise2.
+  Require Import
+    Coq.Vectors.Vector
+    MathClasses.theory.dec_fields
+    Field.
+
+    Context F `{DecField F}.
+    Add Field F: (stdlib_field_theory F).
+
+    Definition vec := Vector.t F.
+
+    Definition vec_constant {n : nat} (c : F) : vec n := Vector.const c n.
+
+    Local Instance vec_zero {n : nat} : Zero (vec n) := Vector.const 0 n.
+
+    Local Instance vec_eq {n : nat} : Equiv (vec n) :=
+      fold_right2 (fun α β S => α = β /\ S) True n.
+
+    Instance: forall {n : nat}, Reflexive (vec_eq (n := n)).
+    Proof.
+      intros n x.
+      induction x.
+      - simpl. trivial.
+      - split; [reflexivity | assumption].
+    Qed.
+
+    Lemma vec_n0_eq_empty : forall (v : vec 0), v = [].
+    Proof.
+      pose (f := fun v => v = []).
+      pose (P := case0 f).
+    Qed.
+  End i.
+
+End Exercise2.
